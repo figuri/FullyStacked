c172880888a540258a1b06ab18be6e5bba4c04b7
unknown <samtisme@gmail.com>
2023-11-06 15:56:56 -0800

    added page files for jsx components

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./index.css";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 

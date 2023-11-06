4fa2b6553797a8bbc4d765b07310bfcd75218af2
unknown <samtisme@gmail.com>
2023-11-06 15:52:26 -0800

    wrote code for app file to structure page, added script cdns for javascript code in html

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./index.css";
+import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

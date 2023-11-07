b99982dccc2ec540f6df1d77ce9af926ee6d4a0b
unknown <samtisme@gmail.com>
2023-11-06 16:28:30 -0800

    made footer for social links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <p>Visit me on linkedin or check out my github with these links</p>
+                <div>
+                    <a href='https://github.com/figuri'>Github</a>
+                    <a href='https://www.linkedin.com/in/samuel-thomas-b82614183/'>LinkedIn</a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;

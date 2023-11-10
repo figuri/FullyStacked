0427bf8f6282524c916d724f7827ddb7c9e0ae0f
unknown <samtisme@gmail.com>
2023-11-09 23:59:57 -0800

    added a link to the navbar component for homepage to root route

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,9 @@ function NavBar() {
       <h1>Fully Stacked</h1>
       <ul>
         <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
           <Link to="/about">About</Link>
         </li>
         <li>

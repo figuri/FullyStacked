dd1708c32dd6d97a96a21164355e30f81bd32362
unknown <samtisme@gmail.com>
2023-11-10 21:24:16 -0800

    wrote intro to javascript component

--- src/components/pages/JavaScript.jsx
+++ src/components/pages/JavaScript.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export default function Javascript() {
-    
+  return (
+    <section>
+      <div>
+        <h1>JavaScript</h1>
+        <p>
+          JavaScript is a high-level, interpreted programming language that
+          allows developers to create interactive and dynamic content within web
+          browsers. Unlike HTML and CSS, which primarily deal with structure and
+          style, JavaScript provides the ability to add functionality, handle
+          events, manipulate the DOM (Document Object Model), and communicate
+          with servers, making web pages more dynamic and user-friendly. fun
+          fact; this paragraph was written in javascript!
+        </p>
+      </div>
+    </section>
+  );
 }
